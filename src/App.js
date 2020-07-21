26ea104f6893d68ce04ee38e1ac869d9cbfd84b1
ACReeser <rogueconformist@gmail.com>
2020-07-20 19:51:27 -0500

    HTML scaffold

--- src/App.js
+++ src/App.js
@@ -4,21 +4,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="canvas">
+    <div className="world">
+      <img src="http://lorempixel.com/400/400" />
+      <img src="http://lorempixel.com/400/400" />
+      <img src="http://lorempixel.com/400/400" />
+      <img src="http://lorempixel.com/400/400" />
+      <img src="http://lorempixel.com/400/400" />
+      <img src="http://lorempixel.com/400/400" />
+      <img src="http://lorempixel.com/400/400" />
+    </div>
+      <div className="overlay"> 
+        <div className="left">
+          <div className="top">
+            Year 1, 
+            Spring
+          </div>
+          <div className="bottom">
+            <span>
+              <b>Material Capital</b> 55
+            </span>
+            <span>
+              <b>Political Capital</b> 25
+            </span>
+          </div>
+        </div>
+        <div className="right">
+          <div><b>Socopolis</b></div>
+          <div>
+            <b>Sentiment</b>&nbsp;
+            <span>Unhappy (40%)</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
