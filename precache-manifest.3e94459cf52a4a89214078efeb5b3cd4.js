ec9c2b37da7055ae2a44869fc909c4916156dd65
ACReeser <rogueconformist@gmail.com>
2020-09-16 13:27:18 -0500

    Updates

---
+++ precache-manifest.3e94459cf52a4a89214078efeb5b3cd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d832c81b32b6fc1deac281f1a41d0108",
+    "url": "/socsim/index.html"
+  },
+  {
+    "revision": "2137f119aba3af9f3b2b",
+    "url": "/socsim/static/css/main.6ddf3453.chunk.css"
+  },
+  {
+    "revision": "1d45db3f7d0ef08c3a56",
+    "url": "/socsim/static/js/2.d64d293d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/socsim/static/js/2.d64d293d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2137f119aba3af9f3b2b",
+    "url": "/socsim/static/js/main.d3d588c1.chunk.js"
+  },
+  {
+    "revision": "07db42d5db22dc47575b",
+    "url": "/socsim/static/js/runtime-main.a6aea435.js"
+  }
+]);
