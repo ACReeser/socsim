cfb1acfa42a905b4ef0d4e4c55c8fa6d976525cc
Alex Reeser <alexcreeser@gmail.com>
2020-12-09 21:47:14 -0600

    Start on research

--- src/modal-content/Research.tsx
+++ src/modal-content/Research.tsx
@@ -1,15 +1,33 @@
 import React from "react";
+import './research.css';
 
 export class ResearchPanel extends React.Component<{
 
+}, {
 }>{
     render(){
         return <div>
             <div className="col-2">
-            <h2>Research Lab</h2>
-            <div>
-              Currently probing 0 beings
+              <div>
+                <h2>Research Lab</h2>
+
+              </div>
+            <div className="vertical">
+              <div className="text-center">
+                Currently probing 0 beings
+              </div>
+              <div>
+                <span className="victim bean triangle shake">
+                  😨
+                </span>
+              </div>
+              <div>
+
+              </div>
             </div>
+          </div>
+          <div>
+
           </div>
         </div>
     }
