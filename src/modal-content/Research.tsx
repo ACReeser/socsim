bb3c988cb8e32f0fae7574de32a791c0b3df8440
Alex Reeser <alexcreeser@gmail.com>
2020-12-10 22:14:07 -0600

    Research robot

--- src/modal-content/Research.tsx
+++ src/modal-content/Research.tsx
@@ -1,34 +1,88 @@
 import React from "react";
+import { RobotArm } from "../widgets/RobotArm";
 import './research.css';
 
 export class ResearchPanel extends React.Component<{
 
 }, {
+  toolI: number, toolI2: number, toolI3: number, toolI4: number
 }>{
-    render(){
-        return <div>
-            <div className="col-2">
-              <div>
-                <h2>Research Lab</h2>
+  tools = [
+  '📡',
+  '🗜️',
+  '🔬',
+  '💉',
+  '🔎',
+  '🧼',
+  '🧲',
+  '🎥',
+  '🔧',
+  ];
+  constructor(props:any){
+    super(props);
+    this.state = {
+      toolI: 0,
+      toolI2: 2,
+      toolI3: 4,
+      toolI4: 1,
+    };
+  }
+  interval?: number;
+  loop(lastI: number){
+    let i = lastI + 1;
+    if (i === this.tools.length)
+      i = 0;
+    return i;
+  }
+  componentDidMount(){
+    this.interval = window.setInterval(() => {
+      this.setState({
+        toolI: this.loop(this.state.toolI),
+        toolI2: this.loop(this.state.toolI2),
+        toolI3: this.loop(this.state.toolI3),
+        toolI4: this.loop(this.state.toolI4),
+      });
+    }, 1000);
+  }
+  componentWillUnmount(){
+    if (this.interval != null)
+      window.clearInterval(this.interval);
+  }
+  render(){
+      return <div>
+          <div className="col-2">
+            <div>
+              <h2>Research Lab</h2>
 
-              </div>
-            <div className="vertical">
-              <div className="text-center">
-                Currently probing 0 beings
-              </div>
-              <div>
-                <span className="victim bean triangle shake">
-                  😨
-                </span>
-              </div>
-              <div>
-
+            </div>
+          <div className="vertical">
+            <div className="text-center">
+              Currently probing 0 beings
+            </div>
+            <div>
+              <div className="robot">
+                🤖
+                <RobotArm classN="far-left" tool={this.tools[this.state.toolI]}></RobotArm>
+                <RobotArm classN="left" tool={this.tools[this.state.toolI2]}></RobotArm>
+                <RobotArm classN="near-right" tool={this.tools[this.state.toolI3]}></RobotArm>
+                <RobotArm classN="far-right" tool={this.tools[this.state.toolI4]}></RobotArm>
               </div>
             </div>
-          </div>
-          <div>
+            <div className="">
+              <span className="victim bean triangle shake">😨</span>
+              <span className="victim bean triangle shake">😨</span>
+              <span className="victim bean triangle shake">😨</span>
+              <span className="victim bean triangle shake">😨</span>
+              <span className="victim bean triangle shake">😨</span>
+            </div>
+            <div>
 
+            </div>
           </div>
         </div>
-    }
+        <div>
+
+        </div>
+      </div>
+  }
 }
---
+++ src/widgets/RobotArm.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export class RobotArm extends React.Component<{
+    tool: string,
+    classN: string
+}, {
+
+}> {
+    render(){
+        const style = {};
+        return <svg width="100" height="300" xmlns="http://www.w3.org/2000/svg" style={style} className={this.props.classN+" robot-arm"}>
+        <g>
+         <title>robotarm</title>
+         <rect fill="transparent" id="canvas_background" height="302" width="102" y="-1" x="-1"/>
+         <g display="none" overflow="visible" y="0" x="0" height="100%" width="100%" id="canvasGrid">
+          <rect fill="url(#gridpattern)" stroke-width="0" y="0" x="0" height="100%" width="100%"/>
+         </g>
+        </g>
+        <g>
+         <title>Layer 1</title>
+         <rect id="svg_2" height="200" width="48" y="91.63545" x="25" stroke-width="1.5" stroke="#000" fill="#BBBBBB"/>
+         <ellipse ry="48" rx="48" id="svg_1" cy="50" cx="50" stroke-width="1.5" stroke="#000" fill="#999999"/>
+         <ellipse ry="20" rx="20" id="svg_3" cy="50" cx="50" stroke-width="1.5" stroke="#000" fill="#444444"/>
+         <ellipse ry="48" rx="48" id="svg_4" cy="250" cx="50" stroke-width="1.5" stroke="#000" fill="#999999"/>
+        <ellipse ry="20" rx="20" id="svg_5" cy="250" cx="50" stroke-width="1.5" stroke="#000" fill="#444444">{this.props.tool}</ellipse>
+            
+        </g><text y="250" x="-20" fontSize="4em">{this.props.tool}</text>
+       </svg>
+    }
+}
